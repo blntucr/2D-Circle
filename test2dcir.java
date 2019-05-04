4f62c80fce607ec6cff452671a24e37aded1e68b
blntucr <49984122+blntucr@users.noreply.github.com>
2019-05-04 15:23:14 +0300

    Update test2dcir.java

--- test2dcir.java
+++ test2dcir.java
@@ -1,5 +1,5 @@
 
-public class test2dciir {
+public class test2dcir {
 	public static void main(String args[]) {
 		Circle2D circle = new Circle2D(10,10,5);
 		Circle2D circle1 = new Circle2D(18,18,4);
